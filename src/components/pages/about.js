c48e55cbcfd975bab8abbcdc44a1f098f3011eaf
Melisa Figueroa <mel.figueroa1407@gmail.com>
2022-01-31 20:34:41 -0300

    navbar

---
+++ src/components/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function about() {
+  return (
+    <div>
+      <h1>Página sobre nosotros</h1>
+    </div>
+  );
+}
+
+export default about;
---
+++ src/components/pages/products.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function products() {
+  return (
+    <div>
+      <h1>Página sobre productos</h1>
+    </div>
+  );
+}
+
+export default product;
